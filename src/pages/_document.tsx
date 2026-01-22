4edc000594275606e07d544b33cab0ba95350900
bloom-294 <non44921@gmail.com>
2026-01-22 19:41:10 +0900

    link修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,8 @@ const MyDocument = () => {
           rel="stylesheet"
         />
         <link
-          rel="stylesheets"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
+          rel="stylesheet"
         />
         <link
           rel="stylesheet"
