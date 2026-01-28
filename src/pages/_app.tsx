c3ae641ebf7a37f56b37bbb0f60ec5167dba1e63
bloom-294 <non44921@gmail.com>
2026-01-28 22:24:49 +0900

    透過の修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,17 @@ import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import Layout from "components/layout";
 import "swiper/css/bundle";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
+    useEffect(() => {
+      document.body.classList.add('loaded');
+      return () => {
+        document.body.classList.remove('loaded');
+      };
+    }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import { Loader } from "components/Atoms/loader";
 import { Information } from "components/Molecules/Information";
 import { Map } from "components/Molecules/map";
@@ -11,13 +11,6 @@ import { Slide } from "components/Molecules/swiper";
 import { SlideCursor } from "components/Molecules/swiperCursor";
 
 export const Home = ({ data }: any) => {
-
-    useEffect(() => {
-      document.body.classList.add('loaded');
-      return () => {
-        document.body.classList.remove('loaded');
-      };
-    }, []);
 
   if (!data)
     return (
