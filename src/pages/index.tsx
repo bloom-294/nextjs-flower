e7d0be17da8f509caf3104287d1571d0fd883685
bloom-294 <non44921@gmail.com>
2026-05-08 12:56:50 +0900

    末尾スラッシュの有無に依存しないようURLの結合

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,12 +52,15 @@ export const Home = ({ data }: any) => {
 };
 
 export const getStaticProps = async () => {
-
   const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
   if (!API_BASE_URL) {
     throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
   }
-  const res = await fetch(`${API_BASE_URL}/items`);
+
+  const normalizedBaseUrl = API_BASE_URL.endsWith("/")
+    ? API_BASE_URL
+    : `${API_BASE_URL}/`;
+  const res = await fetch(new URL("items", normalizedBaseUrl).toString());
   const json = await res.json();
 
   return {
