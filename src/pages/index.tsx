1aee031f47f37fe30f921786cf35d18853d79c7b
bloom-294 <non44921@gmail.com>
2026-05-08 12:20:32 +0900

    NEXT_PUBLIC_API_BASE_URLが未設定時にエラーが出るよう修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,7 +52,12 @@ export const Home = ({ data }: any) => {
 };
 
 export const getStaticProps = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/items`);
+
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+  if (!API_BASE_URL) {
+    throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
+  }
+  const res = await fetch(`${API_BASE_URL}/items`);
   const json = await res.json();
 
   return {
