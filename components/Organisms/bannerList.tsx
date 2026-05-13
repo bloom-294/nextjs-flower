43c9a0c85202acb1d19644192a5808a6cbcc5363
bloom-294 <non44921@gmail.com>
2026-05-13 19:36:54 +0900

    refactor:BannerList

--- components/Organisms/bannerList.tsx
+++ components/Organisms/bannerList.tsx
@@ -1,25 +1,27 @@
-import React from "react";
 import Image from "next/image";
 
+const images = [
+  "/happyChristmas.jpg",
+  "/happybirthdayBanner.jpg",
+  "/ChristmasBanner.png",
+];
+
 export const BannerList = () => {
-  const images = [
-    "/happyChristmas.jpg",
-    "/happybirthdayBanner.jpg",
-    "/ChristmasBanner.png",
-  ];
   return (
-    <>
-      <div className=" mt-12">
-        <ul>
-          {images.map((image: string, index: number) => {
-            return (
-              <li className="mb-2" key={index}>
-                <Image src={image} width={200} height={150} alt="topImage" />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </>
+    <div className="mt-12">
+      <ul>
+        {images.map((image) => (
+          <li key={image} className="mb-2">
+            <Image
+              src={image}
+              width={200}
+              height={150}
+              alt="バナー画像"
+              sizes="200px"
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
