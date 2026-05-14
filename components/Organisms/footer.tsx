ac51582bc9c5dd5cf766ad9492d8a50bb2047818
bloom-294 <non44921@gmail.com>
2026-05-14 14:38:24 +0900

    refactor:Footer

--- components/Organisms/footer.tsx
+++ components/Organisms/footer.tsx
@@ -1,48 +1,61 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export const Footer = () => {
-  const Twitter = "/logo/Twitter.png";
-  const Instagram = "/logo/Instagram.png";
-  const Facebook = "/logo/facebook.png";
+const snsList = [
+  {
+    href: "https://twitter.com/",
+    src: "/logo/Twitter.png",
+    alt: "twitter",
+  },
+  {
+    href: "https://www.instagram.com/",
+    src: "/logo/Instagram.png",
+    alt: "instagram",
+  },
+  {
+    href: "https://ja-jp.facebook.com/",
+    src: "/logo/facebook.png",
+    alt: "facebook",
+  },
+];
 
+const footerMenuList = [
+  "会社概要",
+  "採用情報",
+  "利用規約",
+  "プライバシーポリシー",
+  "特定商取引法に基づく表示",
+];
+
+export const Footer = () => {
   return (
     <>
-      <div className=" flex flex-wrap justify-center items-center mx-auto py-5 px-5  bg-gray-50">
-        <ul>
-          <li className="float-left mx-5 ">
-            <Link href="https://twitter.com/">
-              <a>
-                <Image src={Twitter} alt="twitter" width={50} height={50} />
-              </a>
-            </Link>
-          </li>
-          <li className="float-left mx-5">
-            <Link href="https://www.instagram.com/">
-              <a>
-                <Image src={Instagram} alt="instagram" width={50} height={50} />
-              </a>
-            </Link>
-          </li>
-          <li className="float-left mx-5">
-            <Link href="https://ja-jp.facebook.com/">
-              <a>
-                <Image src={Facebook} alt="facebook" width={50} height={50} />
-              </a>
-            </Link>
-          </li>
+      <div className="mx-auto flex flex-wrap items-center justify-center bg-gray-50 px-5 py-5">
+        <ul className="flex flex-wrap items-center justify-center">
+          {snsList.map((sns) => (
+            <li key={sns.alt} className="mx-5">
+              <Link href={sns.href}>
+                <Image
+                  src={sns.src}
+                  alt={sns.alt}
+                  width={50}
+                  height={50}
+                />
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
-      <div className=" sm:block sm:flex flex-wrap justify-center items-center mx-auto py-5 px-5  bg-gray-50 hidden">
-        <ul className="text-gray-400">
-          <li className="float-left mx-5">会社概要</li>
-          <li className="float-left mx-5">採用情報</li>
-          <li className="float-left mx-5">利用規約</li>
-          <li className="float-left mx-5">プライバシーポリシー</li>
-          <li className="float-left mx-5">特定商取引法に基づく表示</li>
+
+      <div className="mx-auto hidden flex-wrap items-center justify-center bg-gray-50 px-5 py-5 sm:flex">
+        <ul className="flex flex-wrap items-center justify-center text-gray-400">
+          {footerMenuList.map((menu) => (
+            <li key={menu} className="mx-5">
+              {menu}
+            </li>
+          ))}
         </ul>
       </div>
-      {/* </nav> */}
     </>
   );
 };
