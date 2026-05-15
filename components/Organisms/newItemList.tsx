6decd94e404db182fa40bf74e3d717020b35ab26
bloom-294 <non44921@gmail.com>
2026-05-15 11:31:10 +0900

    refactor:NewItemsList

--- components/Organisms/newItemList.tsx
+++ components/Organisms/newItemList.tsx
@@ -2,21 +2,21 @@ import useSWR from "swr";
 import { ItemCardsWrapRecognize } from "components/Organisms/itemCards-wrap";
 import { ItemCardsWrapRecognizeSqlTypes } from "types/type";
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+if (!API_BASE_URL) {
+  throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
+}
+
+const normalizedBaseUrl = API_BASE_URL.endsWith("/")
+  ? API_BASE_URL
+  : `${API_BASE_URL}/`;
+
+const endpoint = new URL("items", normalizedBaseUrl).toString();
+
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const NewItemsList = () => {
-
-  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-  if (!API_BASE_URL) {
-    throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
-  }
-
-  const normalizedBaseUrl = API_BASE_URL.endsWith("/")
-    ? API_BASE_URL
-    : `${API_BASE_URL}/`;
-
-  const endpoint = new URL("items", normalizedBaseUrl).toString();
-
   const { data, error } = useSWR<ItemCardsWrapRecognizeSqlTypes[]>(
     endpoint,
     fetcher
