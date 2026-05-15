86e64f1b2f50dac0b57dd9904f60cce3e9e1888b
bloom-294 <non44921@gmail.com>
2026-05-15 11:41:00 +0900

    refactor:SearchNavigationbar

--- components/Organisms/searchNavigationbar.tsx
+++ components/Organisms/searchNavigationbar.tsx
@@ -4,12 +4,10 @@ import { BannerList } from "./bannerList";
 
 export const SearchNavigationbar = () => {
   return (
-    <>
-      <div className="md:mr-2 mt-2 pr-24 pt-12  xl:w-72 ">
-        <SearchCategoryList />
-        <BannerList />
-        {/* <Calendar /> */}
-      </div>
-    </>
+    <div className="mt-2 pr-24 pt-12 md:mr-2 xl:w-72">
+      <SearchCategoryList />
+      <BannerList />
+      {/* <Calendar /> */}
+    </div>
   );
 };
